7bd3539837485222d998bf27bba9512b7324bc3e
houk-ms <honc@microsoft.com>
2023-03-14 13:53:11 +0800

    add container operations in pages

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TestProject.Pages
@@ -6,14 +9,32 @@ namespace TestProject.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly BlobContainerClient _blobContainerClient;
+        public string ContainerName { get; set; } = string.Empty;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, BlobContainerClient blobContainerClient)
         {
             _logger = logger;
+            _blobContainerClient = blobContainerClient;
         }
 
         public void OnGet()
         {
+            try
+            {
+                if (_blobContainerClient != null)
+                {
+                    var containers = _blobContainerClient.GetBlobs().ToList();
+                    if (containers.Any())
+                    {
+                        ContainerName = containers[0].Name;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ContainerName = ex.ToString();
+            }
 
         }
     }
